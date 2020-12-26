984f314147a66ddb6591bb9fbf877805e896ffb9
enalmarzuki <40693134+enalmarzuki@users.noreply.github.com>
2020-12-26 21:28:30 +0800

    fix bug in bookingform

--- src/config/axios/index.js
+++ src/config/axios/index.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 import errorResponseHandler from "./errorResponseHandler";
 
-const instance = axios.create({
-  baseURL: `${process.env.REACT_APP_HOST}/api/v1/member`,
-});
+// const instance = axios.create({
+//   baseURL: `${process.env.REACT_APP_HOST}/api/v1/member`,
+// });
 
 instance.interceptors.response.use(
   (response) => response,
